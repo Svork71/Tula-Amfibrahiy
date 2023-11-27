bcf9d5e1f9daaf1c4affe992a8de3fd578fba6c4
Svork71 <andrey71anahov@gmail.com>
2023-11-27 19:50:47 +0300

    kok

--- main.py
+++ main.py
@@ -1,8 +1,31 @@
-def main():
-    print('My first git program')
-    print('And I change it every day')
-    print('lol')
+import random
+import PyQt5
+import sys
+
+from PyQt5.QtGui import QPainter, QColor
+from PyQt5 import uic
+from PyQt5.QtWidgets import QPushButton, QWidget, QApplication
+
+class Example(QWidget):
+    def __init__(self):
+        super().__init__()
+        uic.loadUi('UI.ui', self)
+
+    def paintEvent(self, event):
+        qp = QPainter()
+        qp.begin(self)
+        self.draw_flag(qp)
+        qp.end()
+
+    def draw_flag(self, qp):
+        d = random.randint(10, 100)
+        qp.setBrush(QColor(255, 0, 0))
+        qp.drawEllipse(d, d, d, d)
 
 
 if __name__ == '__main__':
-    main()
+    app = QApplication(sys.argv)
+    ex = Example()
+    ex.show()
+    sys.exit(app.exec())
+
